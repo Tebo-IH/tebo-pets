d1073e004bf110d33869b45f93483dfe1cb10947
Borja del Amo <borjadao@gmail.com>
2020-02-19 19:33:30 +0100

    authentication working

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema(
+  {
+    username: String,
+    password: String
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+);
+
+module.exports = mongoose.model("user", schema);
+
